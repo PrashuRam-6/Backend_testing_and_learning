2c93ee9aa5458e9cbbe6a792c05f50433256e444
Prashant Thakur <prashant.thakurnp2@gmail.com>
2026-03-12 19:34:40 +0530

    Learnt about some tokens bro

--- chapter 2/src/routes/authRoutes.js
+++ chapter 2/src/routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import db from '../db.js'
+import bcrypt from 'bcryptjs'
 
 const router = express.Router()
 
@@ -11,8 +12,30 @@ router.post('/register',(req,res)=>{
 
     const {username , password} = req.body
     console.log(username,password);
-    res.sendStatus(200)
-    
+   
+
+    //encrypting the password baby
+
+    const hashedPassword = bcrypt.hashSync(password,8)
+    console.log(hashedPassword)    
+
+    try{
+
+        const insertUser = db.prepare(`INSERT INTO user (username,password) VALUES (?,?)`)
+        const result = insertUser.run(username,hashedPassword)
+         
+
+
+         // Create a token baby
+
+         const token = jwt.sign({id:result.lastInsertRowid}, process.env.JWT_SECRET_KEY,{expiresIn : '24h'})
+         res.json({token})
+
+    }catch(error)
+    {
+        console.log(error);
+        res.sendStatus(503)
+    }
 
 
 })
